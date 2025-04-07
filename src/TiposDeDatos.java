9ed9cf9687d9c772f31486571a8b09d855ff1d49
alansastre <alan@alansastre.co>
2025-04-07 11:46:02 +0200

    Primer commit hola mundo

---
+++ src/TiposDeDatos.java
@@ -0,0 +1,19 @@
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        // Comentario
+
+        // Crear una variable edad:
+
+        int edad = 25;
+        System.out.println(edad);
+
+        double altura = 1.60;
+
+        String nombre = "Alan";
+
+
+    }
+
+}
