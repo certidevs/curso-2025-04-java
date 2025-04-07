89988420506f69ad7a937c38a3ee322ec1c39157
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-04-07 12:26:03 +0200

    tipos de datos primitivos

--- src/TiposDeDatos.java
+++ src/TiposDeDatos.java
@@ -13,6 +13,34 @@ public class TiposDeDatos {
 
         String nombre = "Alan";
 
+        // Tipos enteros
+
+        byte planta = 2;
+
+        short salario = 2000;
+
+        int numero = 12345689;
+
+        long numeroGrande = 1000L;
+
+        // Tipos de punto flotante (decimales)
+
+        float decimal = 1.5f;
+
+        double decimalPreciso = 1.55555;
+
+        // Tipo carácter
+
+        char caracter = 'a';
+        char cifra = '2';
+
+        // Tipo booleano
+
+        boolean premium = true;
+
+        // Se pueden añadir guiones bajos para facilitar la lectura
+
+        long numeroMuyGrande = 1_234_567_890_123L;
 
     }
 
