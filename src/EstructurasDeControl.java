2780b25dec033e62206da6c7dcb6ddc2c578d645
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-04-09 11:48:54 +0200

    estructuras de control switch

--- src/EstructurasDeControl.java
+++ src/EstructurasDeControl.java
@@ -75,5 +75,125 @@ public class EstructurasDeControl {
         mensajeTer = (edadTer >= 18) ? "Mayor de edad" : "Menor de edad";
         System.out.println(mensajeTer);
 
+        System.out.println("\n");
+
+        // switch
+
+        int diaSwitch = 14;
+        String nombreDiaSwitch;
+
+        switch (diaSwitch) {
+            case 1:
+                nombreDiaSwitch = "Lunes";
+                break;
+            case 2:
+                nombreDiaSwitch = "Martes";
+                break;
+            case 3:
+                nombreDiaSwitch = "Miércoles";
+                break;
+            case 4:
+                nombreDiaSwitch = "Jueves";
+                break;
+            case 5:
+                nombreDiaSwitch = "Viernes";
+                break;
+            case 6:
+                nombreDiaSwitch = "Sábado";
+                break;
+            case 7:
+                nombreDiaSwitch = "Domingo";
+                break;
+            default:
+                nombreDiaSwitch = "día inválido";
+        }
+        System.out.println("El día " + diaSwitch + " es " + nombreDiaSwitch);
+
+        // switch con String
+        String fruta = "MANZANA";
+
+        switch (fruta.toLowerCase()) {
+            case "manzana":
+                System.out.println("Las manzanas son rojas o verdes");
+                break;
+            case "platano":
+                System.out.println("Los plátanos son amarillos");
+                break;
+            default:
+                System.out.println("No conozco este tipo de fruta");
+        }
+
+        // switch con enumeraciones
+
+        enum DiaSemana { LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO }
+
+        DiaSemana hoy = DiaSemana.MIERCOLES;
+
+        switch (hoy) {
+            case LUNES:
+            case MARTES:
+            case MIERCOLES:
+            case JUEVES:
+            case VIERNES:
+                System.out.println("Es un día laboral");
+                break;
+            case SABADO:
+            case DOMINGO:
+                System.out.println("Es fin de semana");
+                break;
+        }
+
+        int diaMej = 3;
+        String nombreDiaMej = switch (diaMej) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            case 6 -> "Sábado";
+            case 7 -> "Domingo";
+            default -> "día inválido";
+        };
+
+        System.out.println(diaMej + ": " + nombreDiaMej);
+
+        // multiple casos
+        DiaSemana hoyMej = DiaSemana.MIERCOLES;
+
+        String tipoDeJornada = switch (hoyMej) {
+            case LUNES, MARTES, MIERCOLES, JUEVES, VIERNES -> "Día laborable";
+            case SABADO, DOMINGO -> "Fin de semana";
+        };
+
+        System.out.println(hoy + " es " + tipoDeJornada);
+
+        // bloques de código con switch mejorado
+        int mesMej = 7;
+        String estacion = switch (mesMej) {
+            case 12, 1, 2 -> {
+                System.out.println("Hace frío");
+                yield "Invierno";
+            }
+            case 3, 4, 5 -> {
+                System.out.println("Las flores florecen");
+                yield "Primavera";
+            }
+
+            case 6, 7, 8 -> {
+                System.out.println("Hace calor");
+                yield "Verano";
+            }
+            case 9, 10, 11 -> {
+                System.out.println("Las hojas caen");
+                yield "Otoño";
+            }
+            default -> {
+                System.out.println("Mes inválido");
+                yield "Desconocido";
+            }
+        };
+
+
+        
     }
 }
